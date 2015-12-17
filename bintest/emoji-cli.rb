39c84bee38311c8504f9124491bbdcf35bf166bb
Mu-An Chiou <me@muanchiou.com>
2015-12-17 19:22:21 +0900

    So if xclip works this should pass

--- bintest/emoji-cli.rb
+++ bintest/emoji-cli.rb
@@ -2,16 +2,16 @@ require 'open3'
 
 BIN_PATH = File.join(File.dirname(__FILE__), "../mruby/bin/emoji-cli")
 
-assert('hello') do
+assert('poop') do
   output, status = Open3.capture2(BIN_PATH, "poop")
 
   assert_true status.success?, "Process did not exit cleanly"
-  assert_include output, "Hello World"
+  assert_include output.force_encoding("utf-8"), "Copied 💩 !"
 end
 
 assert('version') do
   output, status = Open3.capture2(BIN_PATH, "version")
 
   assert_true status.success?, "Process did not exit cleanly"
-  assert_include output, "v0.0.1"
+  assert_include output, "v0.0.3"
 end
